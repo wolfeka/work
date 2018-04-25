d444389eed9ac67c8fadbd1f5104cdfc61f03015
Kailey <wolfeka@g.cofc.edu>
2018-04-25 00:13:24 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+
+namespace salesforceQuery
+{
+    public class Program
+    {
+        private static SalesforceClient CreateClient()
+        {
+            return new SalesforceClient
+            {
+                Username = ConfigurationManager.AppSettings["username"],
+                Password = ConfigurationManager.AppSettings["password"],
+                Token = ConfigurationManager.AppSettings["token"],
+                ClientId = ConfigurationManager.AppSettings["clientId"],
+                ClientSecret = ConfigurationManager.AppSettings["clientSecret"]
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            var client = CreateClient();
+
+            if (args.Length > 0)
+            {
+                client.Login();
+                Console.WriteLine(client.Query(args[0]));
+            }
+             else
+             {
+              client.Login();
+             Console.WriteLine(client.Describe("Account"));
+             Console.WriteLine(client.Describe("Contact"));
+             Console.WriteLine(client.QueryEndpoints());
+             Console.WriteLine(client.Query("SELECT Name from Contact"));
+
+             }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SalesforceClient.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+namespace salesforceQuery
+{
+    public class SalesforceClient
+    {
+        private const string LOGIN_ENDPOINT = "https://login.salesforce.com/services/oauth2/token";
+        private const string API_ENDPOINT = "/services/data/v36.0/";
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string AuthToken { get; set; }
+        public string InstanceUrl { get; set; }
+
+        static SalesforceClient()
+        {
+            // SF requires TLS 1.1 or 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+        }
+
+
+        public void Login()
+        {
+            String jsonResponse;
+            using (var client = new HttpClient())
+            {
+                var request = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"grant_type", "password"},
+                        {"client_id", ClientId},
+                        {"client_secret", ClientSecret},
+                        {"username", Username},
+                        {"password", Password + Token}
+                    }
+                );
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.PostAsync(LOGIN_ENDPOINT, request).Result;
+                jsonResponse = response.Content.ReadAsStringAsync().Result;
+            }
+            Console.WriteLine($"Response: {jsonResponse}");
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
+            AuthToken = values["access_token"];
+            InstanceUrl = values["instance_url"];
+        }
+
+        public string QueryEndpoints()
+        {
+            using (var client = new HttpClient())
+            {
+                string restQuery = InstanceUrl + API_ENDPOINT;
+                var request = new HttpRequestMessage(HttpMethod.Get, restQuery);
+                request.Headers.Add("Authorization", "Bearer " + AuthToken);
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        public string Describe(string sObject)
+        {
+            using (var client = new HttpClient())
+            {
+                string restQuery = InstanceUrl + API_ENDPOINT + "sobjects/" + sObject;
+                var request = new HttpRequestMessage(HttpMethod.Get, restQuery);
+                request.Headers.Add("Authorization", "Bearer " + AuthToken);
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        public string Query(string soqlQuery)
+        {
+            using (var client = new HttpClient())
+            {
+                string restRequest = InstanceUrl + API_ENDPOINT + "query/?q=" + soqlQuery;
+                var request = new HttpRequestMessage(HttpMethod.Get, restRequest);
+                request.Headers.Add("Authorization", "Bearer " + AuthToken);
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+
+}
+
